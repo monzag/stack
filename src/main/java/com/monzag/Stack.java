e80378ddafb425d22e6c04bacb6518cbac625ce2
monzag <zaganiacz.m@gmail.com>
2018-01-27 17:26:34 +0100

    Add constructor

--- src/main/java/com/monzag/Stack.java
+++ src/main/java/com/monzag/Stack.java
@@ -1,6 +1,14 @@
 package com.monzag;
 
-public class Stack {
+public class Stack<T> {
 
-    
+    private int size;
+    private T[] elements;
+    private int top;
+
+    public Stack(int size) {
+        this.size = size;
+        this.elements = (T[]) new Object[size];
+        this.top = -1;
+    }
 }
