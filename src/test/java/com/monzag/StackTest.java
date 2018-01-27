044fc83fd8fc147ab884f624ec5f9bf0bdb9d73e
monzag <zaganiacz.m@gmail.com>
2018-01-27 17:59:37 +0100

    Add createStack and addDataToStack methods

--- src/test/java/com/monzag/StackTest.java
+++ src/test/java/com/monzag/StackTest.java
@@ -1,7 +1,24 @@
 package com.monzag;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StackTest {
+
+    private Stack<Integer> stack;
+
+    @BeforeEach
+    void createStack() {
+        this.stack = new Stack(3);
+    }
+
+
+    private void addDataToStack() throws StackOverFlowException {
+        for (Integer i = 0; i < 3; i++) {
+            stack.push(i);
+        }
+    }
 
 }
